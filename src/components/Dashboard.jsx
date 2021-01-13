c1b995b39fa9e9ca5f9392070c68b5da51bb80ad
Dhanush <sdhanush163@gmail.com>
2021-01-14 00:42:26 +0530

    [Frontend] Added Dashboard state

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import data from "../data/imdb_movie_analysis_data.json";
 
 class DashBoard extends Component {
+    state = {
+        data: [],
+    };
+    componentDidMount() {
+        this.setState({
+            data,
+        });
+    }
     render() {
         return <div>This is the dashboard</div>;
     }
